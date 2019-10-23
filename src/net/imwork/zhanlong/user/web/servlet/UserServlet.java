b25635f144c447b291a7fce0d2bfe4b3348a5da7
展龙 <120769563@qq.com>
2019-10-23 08:58:20 +0800

    创建用户模块包结构

---
+++ src/net/imwork/zhanlong/user/web/servlet/UserServlet.java
@@ -0,0 +1,18 @@
+package net.imwork.zhanlong.user.web.servlet;
+
+import net.imwork.zhanlong.servlet.BaseServlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "UserServlet",urlPatterns = "/UserServlet")
+public class UserServlet extends BaseServlet
+{
+    public String regist(HttpServletRequest request, HttpServletResponse response)
+    {
+
+        return null;
+    }
+
+}
